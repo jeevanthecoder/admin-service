1d64083aee139096beb280b048e66cb2c01c41df
jeevanthecoder <98173940+jeevanthecoder@users.noreply.github.com>
2024-01-19 20:27:01 +0530

    Add files via upload

---
+++ admin-service/src/main/java/com/example/adminservice/entity/TrainDetails.java
@@ -0,0 +1,54 @@
+package com.example.adminservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.Set;
+
+@Table
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TrainDetails {
+
+    @Id
+    @GeneratedValue
+    public int t_Id;
+
+    @Column(unique = true)
+    public String trainNumber;
+
+    @Column(unique = true)
+    public String trainName;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JsonManagedReference
+    public Set<ClassType> classType;
+
+    public String source;
+    public String dest;
+
+    public Date dateOfJourney;
+
+    @ManyToOne()
+    @JsonBackReference
+    private AdminAccess adminAccess;
+
+    public TrainDetails(String trainNumber, String trainName, Set<ClassType> classType, String source, String dest, Date dateOfJourney, AdminAccess adminAccess) {
+        this.trainNumber = trainNumber;
+        this.trainName = trainName;
+        this.classType = classType;
+        this.source = source;
+        this.dest = dest;
+        this.dateOfJourney = dateOfJourney;
+        this.adminAccess = adminAccess;
+    }
+}
